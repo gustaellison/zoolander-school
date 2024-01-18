102b87a83fdf10b0d83da502d99558a43dc5bf1d
kfblack <kalinblack21@gmail.com>
2024-01-18 11:59:34 -0500

    Student CRUD

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('students/', views.student_index, name='student_index'),
     path('students/create/', views.StudentCreate.as_view(), name='student_create'),
+    path('students/<int:pk>/update/', views.StudentUpdate.as_view(), name='student_update'),
+    path('students/<int:pk>/delete/', views.StudentDelete.as_view(), name='student_delete'),
     path('students/<int:student_id>/', views.student_detail, name="student_detail"),
     path('accounts/signup/', views.signup, name='signup'),
 ]
