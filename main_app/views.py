c88b9a2432790fdf40429a7a8d2cb9177c7fd23d
kfblack <kalinblack21@gmail.com>
2024-01-17 21:19:18 -0500

    Student creation form and views of student list

--- main_app/views.py
+++ main_app/views.py
@@ -8,8 +8,13 @@ def home(request):
     return render(request, 'home.html')
 
 def student_index(request):
-    return render(request, 'students/index.html')
+    students = Student.objects.all()
+    return render(request, 'students/index.html', {'students': students})
+
+def student_detail(request, student_id):
+    student = Student.objects.get(id=student_id)
+    return render(request, 'students/detail.html', {'student': student})
 
 class StudentCreate(CreateView):
     model = Student
-    fields = ["name"]
+    fields = '__all__'
