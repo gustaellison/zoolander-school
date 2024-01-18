7d9847319d44840acaac3ddeebc5f6eeea02fcfb
kfblack <kalinblack21@gmail.com>
2024-01-18 10:45:26 -0500

    Student Details View

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('students/', views.student_index, name='student_index'),
     path('students/create/', views.StudentCreate.as_view(), name='student_create'),
-    path('students/<int:id>/', views.student_detail, name="student_detail"),
+    path('students/<int:student_id>/', views.student_detail, name="student_detail"),
     path('accounts/signup/', views.signup, name='signup'),
 ]
