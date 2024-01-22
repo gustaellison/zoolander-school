aff2755fad1f0088021946bb3ee8c3c7d06103c2
joshuawynn <joshua.a.wynn@gmail.com>
2024-01-22 09:39:46 -0500

    removed add and delete teacher functionality

--- zoolander/settings.py
+++ zoolander/settings.py
@@ -79,7 +79,10 @@ WSGI_APPLICATION = 'zoolander.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'zoolander'
+        'NAME': 'zoolander',  # Your PostgreSQL username
+        'PASSWORD': 'chr0me',  # Your PostgreSQL password
+        'HOST': 'localhost',  # Set the host to the location of your PostgreSQL server
+        'PORT': '5432',  # Set the port to the PostgreSQL port
     }
 }
 
